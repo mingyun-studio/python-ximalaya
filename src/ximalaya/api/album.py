4b4ed7afacdfd92b3565a59708172b40c4060611
Garrett HE <garrett.he@outlook.com>
2024-01-22 10:31:02 +0800

    Add functions to get category albums

--- src/ximalaya/api/album.py
+++ src/ximalaya/api/album.py
@@ -1,4 +1,6 @@
-from typing import TypedDict
+from typing import TypedDict, Iterator
+
+from ximalaya.client import XimalayaClient
 
 
 class SubscriptInfo(TypedDict):
@@ -34,3 +36,29 @@ class AlbumsResponse(TypedDict):
     pageNum: int
     pageSize: int
     albums: list[AlbumInfo]
+
+
+class AlbumsResponsePagination(Iterator):
+    client: XimalayaClient
+    path: str
+    page_num: int
+
+    def __init__(self, client: XimalayaClient, path: str):
+        self.client = client
+        self.path = path
+        self.page_num = 1
+
+    def __next__(self) -> list[AlbumInfo]:
+        res: AlbumsResponse = self.client.request(f'{self.path}&pageNum={self.page_num}')['data']
+        albums = res['albums']
+
+        if len(albums) == 0 or res['total'] == 0:
+            raise StopIteration()
+
+        self.page_num += 1
+
+        return albums
+
+
+def get_category_albums(client: XimalayaClient, category_id: int, page_size: int = 56, sort_by: int = 1) -> AlbumsResponsePagination:
+    return AlbumsResponsePagination(client=client, path=f'/revision/category/v2/albums?pageSize={page_size}&sort={sort_by}&categoryId={category_id}')
